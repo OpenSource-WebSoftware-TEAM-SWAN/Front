51c8b7bb1f3f57b96fae188c5f9f04b8708ea03a
nam3762 <nam37623762@gmail.com>
2023-05-21 18:16:38 +0900

    2023-05-21 시드보는창 디자인 변경/navbar 추가

--- bucket.js
+++ bucket.js
@@ -22,4 +22,30 @@
         $("#btn1").click(function () {
         $("#so1").remove();
     });
+
+    // nav-btn 클릭 시 main-nav 슬라이드 애니메이션 효과
+    $(".nav-btn").click(function () {
+        $(".main-nav").addClass("slide-in active");
+        $("#Article2").addClass("slide-in active");
     });
+
+    // 메인 네비게이션 외 다른 부분 클릭 시 메인 네비게이션 닫기
+    $(document).mouseup(function (e) {
+        var container = $(".main-nav");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $(".main-nav").removeClass("active");
+        }
+    });
+
+    $("#images-modal").load("./imagesModal.html");
+
+
+    // add toogle
+    $('.add-title').click(function(){
+        var toggle=document.createElement("div")
+        toggle.classList.add("first-toggle");
+        toggle.innerHTML="SEX"
+        document.getElementById("headline").appendChild(toggle);
+    });
+    });
+
