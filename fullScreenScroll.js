cb891335cb3377bc06f7ed868de3ed782ee13473
nam3762 <nam37623762@gmail.com>
2023-06-05 19:08:23 +0900

    2023-06-05 change

--- fullScreenScroll.js
+++ fullScreenScroll.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   var viewportWidth = $(window).width();
   var responsiveWidth = viewportWidth * 0.5; // 50%로 설정 예시
+  var g_interval;
   $("#fullpage").fullpage({
     navigation: true, // 페이지 인디케이터 활성화
     navigationPosition: "right", // 페이지 인디케이터 위치 (오른쪽)
@@ -8,7 +9,7 @@ $(document).ready(function () {
     showActiveTooltip: true, // 현재 활성화된 섹션의 툴팁 표시
     scrollingSpeed: 1000, // 스크롤 속도 (1초)
     easingcss3: "cubic-bezier(0.86, 0, 0.07, 1)", // 스크롤 이동에 사용할 easing 효과
-    responsiveWidth: responsiveWidth, // 뷰포트 너비가 768px 이하일 때 응답형 동작
+    responsiveWidth: responsiveWidth, // 뷰포트 너비가 화면 너비 50% 이하일 때 응답형 동작
     controlArrows: true,
   });
 });
